d0c8bc29a28263aeed3375d14d1d9588a7465536
brickgao <brickgao@gmail.com>
2014-03-20 15:30:09 +0800

    add wave_op

--- checking_frame_offsets/wave_op.py
+++ checking_frame_offsets/wave_op.py
@@ -2,15 +2,22 @@
 
 import wave
 
-def add_zero(_file_name, offset):
+def get_nframe(_file_name)
+    _file_in = wave.open(_file_name, 'rb')
+    _tmp = _file_in.getparams()
+    return _tmp[3] / 576
+
+def add_zero(_file_name, pos, offset):
     _file_in = wave.open(_file_name, 'rb')
     _tmp = _file_in.getparams()
     _params = (_tmp[0], _tmp[1], _tmp[2], _tmp[3] + offset, _tmp[4], _tmp[5])
     _data = _file_in.readframes(_tmp[3])
+    _add = ''
     for _i in range(offset):
         for _j in range(_tmp[1]):
-            _data = chr(0) + _data
-    _file_out = wave.open('tmp.wav', 'wb')
+            _add += chr(0)
+    _data = _data[:pos * 576] + _add + _data[pos * 576:]
+    _file_out = wave.open('tmp_processed.wav', 'wb')
     _file_out.setparams(_params)
     _file_out.writeframes(_data)
     _file_out.close()
