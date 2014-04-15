65d93597d0565042653fbb8c5d5c485524717867
brickgao <brickgao@gmail.com>
2014-04-15 23:23:23 +0800

    fix add zero to remove frames

--- checking_frame_offsets/detect.py
+++ checking_frame_offsets/detect.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import wave_op, math, os
+import wave_op, math, os, eyed3
 
 # Decoder path
 _decoder = os.path.abspath(u'./mpg123_decode/mpg123_to_wav.exe')
@@ -9,43 +9,41 @@ _decoder = os.path.abspath(u'./mpg123_decode/mpg123_to_wav.exe')
 _encoder = os.path.abspath(u'./lame_encode/lame.exe')
 
 def detect(_file_name):
-    _ret = []
     _recv = os.popen((_decoder + u' ' + _file_name + u' tmp_origin.wav').encode('gbk')).read()
+    _audio = eyed3.load(_file_name)
+    _audio_bitrate = str(_audio.info.bit_rate[1])
     if 'written' not in _recv:
         print 'Warning: This file is not support'
     else:
-        _nframe = wave_op.get_nframe(u'tmp_origin.wav')
-        for _frame in range(_nframe):
-            print 'dealing with frame ' + str(_frame)
-            _min_num, _offset = [], []
-            for _i in range(576):
-                wave_op.add_zero(u'tmp_origin.wav', _frame, _i)
-                _recv = os.popen(_encoder + u' tmp_processed.wav tmp.mp3 -b 32').read()
-                _len = 0
-                with open(u'data.tmp', 'r') as _file_in:
-                    for _j in range(2):  _line = _file_in.readline()
-                    while True:
-                        _line = _file_in.readline()
-                        if _line == '':     break
-                        if _line == '\n':   continue
-                        _mdct_arr = map(float, _line.split(' ')[:-1])
-                        _cnt = 0
-                        for _j in range(576):
-                            _tmp = 10 * math.log(max((_mdct_arr[_j] ** 2) * (10 ** 10), 1.0), 10)
-                            if _tmp == 0.0:     _cnt += 1
-                        if not _i:
-                            _offset.append(0)
-                            _min_num.append(_cnt)
-                        else:
-                            if _len < len(_min_num) and _cnt < _min_num[_len]:
-                                _min_num[_len], _offset[_len] = _cnt, _i
-                        _len += 1
-                    _file_in.close()
-                os.remove(u'tmp_processed.wav')
-                os.remove(u'tmp.mp3')
-                os.remove(u'data.tmp')
-            _ret.append(_offset[_frame])
-        print _ret
+        _min_num, _offset = [], []
+        for _i in range(576):
+            print 'dealing with ' + str(_i) + ' offset(s)'
+            wave_op.del_frames(u'tmp_origin.wav', _i)
+            _recv = os.popen((_encoder + u' tmp_processed.wav tmp.mp3 -b ' + _audio_bitrate).encode('gbk')).read()
+            _len = 0
+            with open(u'data.tmp', 'r') as _file_in:
+                for _j in range(2):  _line = _file_in.readline()
+                while True:
+                    _line = _file_in.readline()
+                    if _line == '':     break
+                    if _line == '\n':   continue
+                    _mdct_arr = map(float, _line.split(' ')[:-1])
+                    _cnt = 0
+                    for _j in range(576):
+                        _tmp = 10 * math.log(max((_mdct_arr[_j] ** 2) * (10 ** 10), 1.0), 10)
+                        if _tmp != 0.0:     _cnt += 1
+                    if not _i:
+                        _offset.append(0)
+                        _min_num.append(_cnt)
+                    else:
+                        if _len < len(_min_num) and _cnt < _min_num[_len]:
+                            _min_num[_len], _offset[_len] = _cnt, _i
+                    _len += 1
+                _file_in.close()
+            os.remove(u'tmp_processed.wav')
+            os.remove(u'tmp.mp3')
+            os.remove(u'data.tmp')
+        print _min_num, _offset
     os.remove(u'tmp_origin.wav')
             
 
--- checking_frame_offsets/wave_op.py
+++ checking_frame_offsets/wave_op.py
@@ -2,26 +2,18 @@
 
 import wave
 
-def get_nframe(_file_name):
+def del_frames(_file_name, offset):
     _file_in = wave.open(_file_name, 'rb')
     _tmp = _file_in.getparams()
-    return _tmp[3] / 576
-
-def add_zero(_file_name, pos, offset):
-    _file_in = wave.open(_file_name, 'rb')
-    _tmp = _file_in.getparams()
-    _params = (_tmp[0], _tmp[1], _tmp[2], _tmp[3] + offset, _tmp[4], _tmp[5])
     _data = _file_in.readframes(_tmp[3])
-    _add = ''
-    for _i in range(offset):
-        for _j in range(_tmp[1]):
-            _add += chr(0)
-    _data = _data[:pos * 576] + _add + _data[pos * 576:]
-    _file_out = wave.open('tmp_processed.wav', 'wb')
+    print _tmp[3], len(_data) / _tmp[1]
+    _data = _data[_tmp[1] * offset:]
+    _params = (_tmp[0], _tmp[1], _tmp[2], _tmp[3] - offset, _tmp[4], _tmp[5])
+    _file_out = wave.open(u'tmp_processed.wav', 'wb')
     _file_out.setparams(_params)
     _file_out.writeframes(_data)
     _file_out.close()
     _file_in.close()
 
 if __name__ == '__main__':
-    add_zero(u'tmp_origin.wav', 1, 1)
+    del_frames(u'tmp_origin.wav', 60000)
